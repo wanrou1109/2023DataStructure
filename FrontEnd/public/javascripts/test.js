d1521b75816a174bfefe3142de7fb85063d5c856
eason <9338akira@gmail.com>
2023-12-14 12:50:45 +0800

    基本前端架構

--- FrontEnd/public/javascripts/test.js
+++ FrontEnd/public/javascripts/test.js
@@ -1,2 +1,14 @@
-let greeting = "hello";
-console.log(greeting);
+document.getElementById('add-button').addEventListener('click', function(){
+    let firstNum = parseInt(document.getElementById("first-num").value);
+    let secondNum = parseInt(document.getElementById("second-num").value);
+    console.log(firstNum + secondNum);
+    fetch('http://localhost:8080/search?query=' + encodeURIComponent(searchTerm))
+        .then(response => response.json())
+        .then(data => {
+            console.log(data); // 處理返回的數據
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+})
+
